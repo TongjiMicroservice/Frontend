eaf92292d78fee5217fa7021a3181f36012dabed
luxingzhi27 <3523718637@qq.com>
2023-12-06 19:21:33 +0800

    整理项目依赖

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import CSV from "vite-plugin-csv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    CSV(),
   ],
   resolve: {
     alias: {

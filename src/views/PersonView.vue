3d50f0889ae9e95d839916659da8f0f9952fb9db
jingziccc <2602780256@qq.com>
2023-12-30 10:50:14 +0800

    稍微写了个人信息界面和日程

--- src/views/PersonView.vue
+++ src/views/PersonView.vue
@@ -1,11 +1,66 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-
+<div id="container" style="margin:auto; width: 100%; height: 100%;">
+  <div id="header" style="text-align: center;">
+  <h1 style="margin-bottom:0;">个人信息页面</h1>
   </div>
+      <!-- User Avatar Section -->
+    <div id="avatar" style="text-align: center; padding: 20px;">
+      <!-- Display user's avatar/image here -->
+      <img src="/default_avatar.png" alt="User Avatar" style="width: 100px; height: 100px; border-radius: 50%;" />
+    </div>
+    <!-- Profile Information Section -->
+  <div id="personalInfor" style="width: 60%; float: left; height: 100%; box-sizing: border-box;">
+    <!-- Name -->
+    <el-form :model="profile" label-width="80px" style="margin-bottom: 20px;">
+      <el-form-item label="Name">
+        <el-input v-model="profile.name" :disabled="!editMode"></el-input>
+      </el-form-item>
+
+      <!-- Account -->
+      <el-form-item label="Account">
+        <el-input v-model="profile.account" :disabled="!editMode"></el-input>
+      </el-form-item>
+
+        <!-- Password -->
+      <el-form-item label="Password">
+        <el-input v-model="profile.password" :type="showPassword ? 'text' : 'password'" :disabled="!editMode">
+          <template slot="append">
+            <el-button @click="showPassword = !showPassword" icon="el-icon-view"></el-button>
+          </template>
+        </el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+
+  <div id="content" style="width:40%; float:left; height: 100%; box-sizing: border-box;">
+    内容在这里
+  </div>
+
+  <div id="footer" style="clear:both; text-align:center;">
+    版权 © Tongji software 2023
+  </div>
+</div>
+
+
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      profile: {
+        name: 'John Doe',
+        account: 'john@example.com',
+        password: '********', // Initial password
+      },
+      editMode: false,
+      showPassword: false,
+    };
+  },
+};
+</script>
+
+
 <style scoped>
+
 </style>
--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -1,9 +1,45 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <div></div>
-</template>
-
-<style scoped>
-</style>
+    <el-calendar v-model="value">
+      <template slot="date-cell" slot-scope="{ date }">
+        {{ date.getDate() }}
+        <div style="width: 100%;" v-for="item in scheduleData" :key="item.day">
+          <el-tag type="danger" v-if="item.day === day ">
+            {{ item.content }}
+          </el-tag>
+        </div>
+      </template>
+    </el-calendar>
+  </template>
+  
+  <script>
+  export default {
+    name: "MySchedule",
+    data() {
+      return {
+        value: new Date(),
+        scheduleData: [
+          { day: "2023-12-02", content: "学习1" },
+          { day: "2023-01-03", content: "学习2" },
+          { day: "2023-01-04", content: "学习3" },
+          { day: "2023-01-05", content: "学习4" },
+          { day: "2023-01-06", content: "学习5" },
+          { day: "2023-01-07", content: "学习6" },
+        ],
+      };
+    },
+    methods: {
+      formatDate(date) {
+        // Format date as "YYYY-MM-DD"
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any scoped styles if needed */
+  </style>
+  

7f7083af3b749d5a59a471390db064ae16e1abf1
luxingzhi27 <3523718637@qq.com>
2024-01-02 08:36:16 +0800

    暂存

--- src/views/manage/MemberManageView.vue
+++ src/views/manage/MemberManageView.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
-    成员管理
+    <div class="flex flex-col p-3">
+      <el-radio-group v-model="view">
+        <el-radio-button label="manage-member">成员管理</el-radio-button>
+        <el-radio-button label="manage-application">项目管理</el-radio-button>
+      </el-radio-group>
+      <div v-if="view==='manage-member'">
+
+      </div>
+      <div v-if="view==='manage-application'">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 </style>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+const view=ref('manage-member')
+</script>
+
